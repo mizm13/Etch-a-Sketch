a1d690c5ef49b5f7d91fdb2f2577449a22b36a87
mizm13 <mizanmodak@gmail.com>
2024-05-19 23:37:01 -0400

    Cleaned up code & added size, reset changes

--- index.js
+++ index.js
@@ -1,22 +1,68 @@
 
+const size = document.querySelector("#size");
 
-for(let i=0; i<16;i++)
+
+size.addEventListener("click", () => {
+
+    let num = parseInt(prompt("Enter number of squares for new grid (1-100):")); 
+     if(num >= 1 && num <= 100)
+     {
+        const container = document.querySelector("#container");
+
+        while(container.hasChildNodes())
+        {
+            container.removeChild(container.firstChild);
+        }
+
+        gridCreation(num);
+     }
+     else
+     {
+        while(container.hasChildNodes())
+        {
+            container.removeChild(container.firstChild);
+        }
+     }
+});
+
+gridCreation(16);
+
+
+function gridCreation(num)
 {
-    const rowDiv = document.createElement("div");
-    rowDiv.className = "row";
-
-    for(let i =0;i < 16; i++)
+    for(let i=0; i<num;i++)
     {
-        const cellDiv = document.createElement("div");
-        cellDiv.className = "cell";
+        const rowDiv = document.createElement("div");
+        rowDiv.className = "row";
 
-        rowDiv.appendChild(cellDiv);
+        for(let i =0;i < num; i++)
+        {
+            const cellDiv = document.createElement("div");
+            cellDiv.className = "cell";
+
+            rowDiv.appendChild(cellDiv);
+
+            cellDiv.addEventListener("mouseover", ()=> {
+
+                cellDiv.style.backgroundColor = "black";
+            });
+
+            const reset = document.querySelector("#reset");
+
+            //resets grid to be empty
+            reset.addEventListener("click", ()=> {
+
+                cellDiv.style.backgroundColor = "white";
+            });
+
+
+        }
+
+        const container = document.querySelector("#container");
+
+        container.appendChild(rowDiv);
+
 
     }
-
-    const container = document.querySelector("#container");
-
-    container.appendChild(rowDiv);
-
 
 }
